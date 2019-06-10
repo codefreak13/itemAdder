565d4caa517df00b1508601fc921cf35883ac3fb
madeInAba <ezeokecc@gmail.com>
2019-06-10 18:54:21 +0100

    I have been able to fix the primary functionality of the javascript file to my satisfaction. I only need to persist it on the ul now

--- pp.js
+++ pp.js
@@ -1,65 +1,70 @@
 //getting the form
-const form = document.querySelector('#form');
+const form = document.querySelector('form');
 //getting the div for the name display
 let firstName = document.querySelector('#firstName');
 
  //getting the product value
- const product = document.querySelector('#product').value;
+ const product = document.querySelector('#product');
  //getting the product price
- const price = document.querySelector('#price').value;
+ const price = document.querySelector('#price');
  //getting the delivery method
- const delivery = document.querySelector('#delivery').value;
+ const delivery = document.querySelector('#delivery');
 
 const container = document.querySelector('#container');
 
-let card = {
-    productName: product,
-    productPrice: price,
-    deliveryMethod: delivery
-};
 
 
     //adding event listener to the form
     form.addEventListener('submit', tasker);
 
-    document.addEventListener('DOMContentLoaded', persistUL)
+    // document.addEventListener('DOMContentLoaded', persistor)
 
 
 //saveTask function fires when form is submitted
 function tasker(e){
 
-   
+    let card = {
+        productName: product.value,
+        productPrice: price.value,
+        deliveryMethod: delivery.value
+    }
 
     container.style.display = 'flex';
-    // addTask()
-    saveToLS(card);
-    persistUL()   
-    
+    addTask()
+    saveToLS(card);   
+     
+    product.value = '';
+     price.value = '';
+     delivery.value = ''
     e.preventDefault() 
 }
 
-// function addTask(){
-        
-//     let pName = document.createElement('p');
-//     pName.appendChild(document.createTextNode(element.productName));
-//     let pPrice = document.createElement('p');
-//     pPrice.appendChild(document.createTextNode(element.productPrice));
-//     let pMethod = document.createElement('p');
-//     pMethod.appendChild(document.createTextNode(element.deliveryMethod));
-//     let cardHold = document.createElement('div');
-//      cardHold.className = 'cardHold';
-//     cardHold.appendChild(pName);
-//     cardHold.appendChild(pPrice);
-//     cardHold.appendChild(pMethod);
-//     container.appendChild(cardHold);    
-// }
+function addTask(){
+      if(product.value == '' || price.value == '' || delivery.value == ''){
+          alert('please fill in the missing details')
+      }else{
+
+    let pName = document.createElement('p');
+    pName.appendChild(document.createTextNode(product.value));
+    let pPrice = document.createElement('p');
+    pPrice.appendChild(document.createTextNode(price.value));
+    let pMethod = document.createElement('p');
+    pMethod.appendChild(document.createTextNode(delivery.value));
+    let cardHold = document.createElement('div');
+     cardHold.className = 'cardHold';
+    cardHold.appendChild(pName);
+    cardHold.appendChild(pPrice);
+    cardHold.appendChild(pMethod);
+    container.appendChild(cardHold); 
+      }
+}
 
 function saveToLS(value){
     //declaring an empty array to house the object created
 let productArray = [];
 //a conditional for saving and retrieving data from trom the local storage
-   if(product !== '' || price !== '' || delivery !== ''){
-    if(localStorage.getItem('product') === null){
+   if(product.value !== '' || price.value !== '' || delivery.value !== ''){
+    if(localStorage.getItem('product') == null){
         productArray.push(value);
         localStorage.setItem('product', JSON.stringify(productArray));
     }else {
@@ -70,41 +75,44 @@ let productArray = [];
 }
 }
 
-function persistUL(){
-    let localArray;
-    if(localStorage.getItem('product') === null){
-        localArray = [];
-    }else {
-     localArray = JSON.parse(localStorage.getItem('product'));
-    }
+// function persistor(){
+//     let localArray;
+//     if(localStorage.getItem('product') == null){
+//         localArray = [];
+//     }else {
+//      localArray = JSON.parse(localStorage.getItem('product'));
+//      }
 
-   localArray.forEach(function(element){
-    let pName = document.createElement('p');
-    pName.appendChild(document.createTextNode(element.productName));
-    let pPrice = document.createElement('p');
-    pPrice.appendChild(document.createTextNode(element.productPrice));
-    let pMethod = document.createElement('p');
-    pMethod.appendChild(document.createTextNode(element.deliveryMethod));
-    let cardHold = document.createElement('div');
-     cardHold.className = 'cardHold';
-    cardHold.appendChild(pName);
-    cardHold.appendChild(pPrice);
-    cardHold.appendChild(pMethod);
-    container.appendChild(cardHold)
-   });
-}
+//    localArray.forEach(function(element){
+//     let pName = document.createElement('p');
+//     pName.appendChild(document.createTextNode(element.productName));
+//     let pPrice = document.createElement('p');
+//     pPrice.appendChild(document.createTextNode(element.productPrice));
+//     let pMethod = document.createElement('p');
+//     pMethod.appendChild(document.createTextNode(element.deliveryMethod));
+    
+//     const cardHold = document.createElement('div');
+//      cardHold.className = 'cardHold';
+//     cardHold.appendChild(pName);
+//     cardHold.appendChild(pPrice);
+//     cardHold.appendChild(pMethod);
+//     container.appendChild(cardHold)
+//    });
+// }
+
+
 
 const storage = localStorage.getItem('auth');
 
 function save(value){
-  localStorage.setItem("auth", value)
+  localStorage.setItem('auth', value)
   return value
 }
 
 if(storage) {
     firstName.innerHTML += storage
 }else{
-    let name = prompt('what is your name?');
+    let name = prompt('what is your name?')
     if (name == null || name == ''){
         // firstName.style.display = 'none';
         firstName.innerHTML = 'Welcome'
